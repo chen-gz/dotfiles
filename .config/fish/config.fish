bfb14c16367a5b8e2e56927f09e13c3fc6343ee5
Guangzong Chen <chen@ggeta.com>
2024-05-05 14:56:42 -0400

    update package and fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,6 @@ end
 
 fish_add_path ~/.nix-profile/bin
 export GPG_TTY=$(tty) ## for gpg and alacritty
+
+alias wgup='sudo systemctl start wg-quick@wg0'
+alias wgdown='sudo systemctl stop wg-quick@wg0'
