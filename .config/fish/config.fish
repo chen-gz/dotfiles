2a1d6dcc2bca26f3017812f5259e596e2bb8b1df
Guangzong Chen <chen@ggeta.com>
2024-05-06 01:02:15 -0400

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,4 @@ export GPG_TTY=$(tty) ## for gpg and alacritty
 
 alias wgup='sudo systemctl start wg-quick@wg0'
 alias wgdown='sudo systemctl stop wg-quick@wg0'
+alias nixdev='nix-shell -p yadm git gnupg'
