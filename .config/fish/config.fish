b56b150468912039ed3c6bd6defc95cea1f05f08
Guangzong Chen <chen@ggeta.com>
2024-05-06 08:00:18 -0400

    update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
+set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
@@ -7,7 +8,8 @@ export GPG_TTY=$(tty) ## for gpg and alacritty
 
 alias wgup='sudo systemctl start wg-quick@wg0'
 alias wgdown='sudo systemctl stop wg-quick@wg0'
-alias nixdev='nix-shell -p yadm git gnupg'
+alias nixdev='nix-shell -p yadm git gnupg --run fish'
 fish_add_path /home/guangzong/.local/share/JetBrains/Toolbox/scripts
 alias dr='devbox run'
 alias ds='devbox shell'
+
