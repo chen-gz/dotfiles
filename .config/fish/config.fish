203093eb39c80f8e3528a5622d7dd7f6b15b91a1
Guangzong Chen <chen@ggeta.com>
2024-05-06 07:41:52 -0400

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,6 @@ export GPG_TTY=$(tty) ## for gpg and alacritty
 alias wgup='sudo systemctl start wg-quick@wg0'
 alias wgdown='sudo systemctl stop wg-quick@wg0'
 alias nixdev='nix-shell -p yadm git gnupg'
+fish_add_path /home/guangzong/.local/share/JetBrains/Toolbox/scripts
+alias dr='devbox run'
+alias ds='devbox shell'
