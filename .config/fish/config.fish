19498e9b196ced8c703ccc40ad063f7415eeb198
Guangzong Chen <chen@ggeta.com>
2024-05-12 00:29:59 -0400

    mereg

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,13 +4,13 @@ if status is-interactive
 end
 
 fish_add_path ~/.nix-profile/bin
-fish_add_path ~/.cargo/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin/
+fish_add_path /home/guangzong/.local/share/JetBrains/Toolbox/scripts
 export GPG_TTY=$(tty) ## for gpg and alacritty
 
 alias wgup='sudo systemctl start wg-quick@wg0'
 alias wgdown='sudo systemctl stop wg-quick@wg0'
 alias nixdev='nix-shell -p yadm git gnupg --run fish'
-fish_add_path /home/guangzong/.local/share/JetBrains/Toolbox/scripts
 alias dr='devbox run'
 alias ds='devbox shell'
-
