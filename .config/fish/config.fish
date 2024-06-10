7766e182b562d7b8190bfae86f3cb739e74bd0a9
Guangzong Chen <chen@ggeta.com>
2024-06-10 10:24:52 -0400

    update auth ssh keys

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,11 +20,11 @@ alias py12='source ~/.local/share/python312/bin/activate.fish'
 alias cdd='cd ~/Documents'
 
 
-# Start or re-use a gpg-agent.
-gpgconf --launch gpg-agent
 
 # Ensure that GPG Agent is used as the SSH agent
 set -e SSH_AUTH_SOCK
 set -U -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 set -x GPG_TTY (tty)
 
+# Start or re-use a gpg-agent.
+gpg-connect-agent updatestartuptty /bye >/dev/null
