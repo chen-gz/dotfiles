742011b1b7a7294c7cb89b0725b13acfcf39c594
Guangzong Chen <chen@ggeta.com>
2024-07-05 22:53:20 -0400

    update package

--- .backup/backup_user_folder.sh
+++ .backup/backup_user_folder.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 source ~/.restic-env
 fd . ~/Documents/ -t f  > /tmp/file_list.txt
+fd . ~/Pictures/ -t f  >> /tmp/file_list.txt
 restic backup --files-from /tmp/file_list.txt --tag user_folder
