83ac76b0709f5027565810ebb511321eecc65b22
Guangzong Chen <chen@ggeta.com>
2025-07-07 03:58:59 +0000

    update secretes

--- .backup/gserver_backup.sh
+++ .backup/gserver_backup.sh
@@ -7,13 +7,13 @@ restic backup /etc/nginx /etc/letsencrypt/  --tag nginx
 echo "backup vaultwarden"
 restic backup /var/lib/vaultwarden /etc/vaultwarden.env --tag vaultwarden
 
-# backup drone
-echo "backup drone"
-restic backup /var/lib/drone /etc/systemd/system/drone-runner-docker.service.d /etc/systemd/system/drone-runner-exec.service.d /etc/systemd/system/drone.service.d  --tag drone
-
-# backup gitea
-echo "backup gitea"
-restic backup /etc/gitea/app.ini /var/lib/gitea --tag gitea
+# # backup drone
+# echo "backup drone"
+# restic backup /var/lib/drone /etc/systemd/system/drone-runner-docker.service.d /etc/systemd/system/drone-runner-exec.service.d /etc/systemd/system/drone.service.d  --tag drone
+# 
+# # backup gitea
+# echo "backup gitea"
+# restic backup /etc/gitea/app.ini /var/lib/gitea --tag gitea
 
 # backup mariadb
 # refer to https://archive.fosdem.org/2022/schedule/event/mariadb_backup_restic/attachments/slides/5135/export/events/attachments/mariadb_backup_restic/slides/5135/mariabackup_restic.pdf
