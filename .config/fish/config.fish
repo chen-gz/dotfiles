1918a66d9d976646c1d3d1763bd48cd0a509f180
Guangzong Chen <chen@ggeta.com>
2025-10-13 19:18:37 -0700

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 set fish_greeting
 
 # Set PATH
-fish_add_path ~/.nix-profile/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path /opt/homebrew/bin
@@ -9,7 +8,7 @@ fish_add_path /opt/homebrew/bin
 # GPG and SSH agent setup
 set -x GPG_TTY (tty)
 if test (uname) = "Darwin"
-    gpgconf --kill gpg-agent
+    # gpgconf --kill gpg-agent
     set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
     gpg-connect-agent updatestartuptty /bye >/dev/null
 else if test (uname) = "Linux"
@@ -21,20 +20,4 @@ else if test (uname) = "Linux"
 end
 
 # Aliases
-alias dr='devbox run'
-alias ds='devbox shell'
 alias cdd='cd ~/Documents'
-
-# Nix daemon
-if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-  source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-end
-
-# Nix shell indicator in prompt
-function fish_right_prompt
-  if set -q IN_NIX_SHELL
-    set_color -o blue
-    echo '(nix)'
-    set_color normal
-  end
-end
